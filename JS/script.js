bc330a41c08208cbd9436ff7e4624a6622007adf
Daniil <megabaptist17@gmail.com>
2022-03-11 01:17:50 +0300

    Commit message.

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,28 @@
-alert ("Hello World!")
+'use strict'
+
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+
+let appData = {
+    budget: money,
+    expenses: { },
+    optionalExpenses: { },
+    income: { },
+    timeData: time,
+    savings: false
+    };
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце" , ''),
+        b = prompt("Во сколько обойдется?" , '');
+        
+        if ((typeof(a)) === 'string' && (typeof(a)) != null ) {
+           appData.expenses[a] = b; 
+        }
+        
+}
+
+alert(appData.budget / 30);
+
+//console.log (time);
